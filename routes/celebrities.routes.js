618b55889f92b4c714df8a80c4c3db7f62866d19
Javier Godoy <javiergodoymiguez@gmail.com>
2024-05-28 20:56:27 +0200

    javi lab

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 const Celebrity = require("../models/celebritie.model");
 
 router.get("/", (req, res) => {
+    console.log('testing...')
     Celebrity.find().then((data) => {
-        res.render("celebrities", {celebryties: data});
+        res.render("celebrities/celebrities", {celebrities: data});
     });
 });
 
@@ -11,7 +12,7 @@ router.get(`/create`, (req, res) => {
     res.render(`celebrities/new-celebrity`);
 });
 
-router.post(`create`, (req, res) => {
+router.post(`/create`, (req, res) => {
     const { name, occupation, catchPhrase } = req.body;
     Celebrity.create({ name, occupation, catchPhrase })
     .then(() => res.redirect(`/celebrities`))
